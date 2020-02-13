2a9895a846d1b2a6dd9007c4e3048d38c82e0345
Jeremie Bouchard <jeremieb4360@gmail.com>
2020-02-13 15:04:09 -0500

    début boucle

--- js/genElement.js
+++ js/genElement.js
@@ -1,4 +1,10 @@
 function genSlider() {
 var boutonSlider =  document.querySelectorAll(".boutonSlider");
+let sectionSlider = document.querySelector("slides");
 console.log(boutonSlider);
+console.log(sectionSlider);
+
+for(let i=0; i<boutonSlider.length; i++){
+    
+}
 }
